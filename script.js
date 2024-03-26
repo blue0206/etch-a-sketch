15ce91e29b19976ecf66d072232902f472f0d905
Aayush Rai <coolaayushrai@gmail.com>
2024-03-26 12:20:46 +0530

    Add button to set grid-size and add functions to create & delete grid
    Initially the grid had a fixed size of 16. Hence added a size button so
    that the user can set size of their choice (limited to size of 100 to
    avoid delay, freeze, etc.)
    
    Created two size variables; one to store current size and another to store
    new size.
    
    Created a createGrid() function that takes size as input and creates a
    new grid of desired size. No changes made to algorithm.
    
    Created a deleteGrid() function that takes size as input and removes the
    existing grid (by deleting the rows) to make space for new grid.

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 const container = document.querySelector('#container');
+const sizeBtn = document.querySelector('#size');
+let initialSize = 16;
 
-for (let i=0; i<16; i++)
-{
-    let rowDiv = document.createElement('div');
-    rowDiv.setAttribute('class', 'row');
-    container.appendChild(rowDiv);
-    
-    for (let j=0; j<16; j++)
+createGrid(initialSize);
+
+sizeBtn.addEventListener('click', () => {
+    let size = prompt("Input grid size. (Eg: 16 for 16x16 grid)\nMaximum allowed size = 100");
+    while (size > 100)
     {
-        let colDiv = document.createElement('div');
-        colDiv.setAttribute('class', 'unit');
-        rowDiv.appendChild(colDiv);
+        size = prompt("Maximum allowed size is 100.\nKindly input a valid grid size.");
+    }
+    deleteGrid(initialSize);
+    createGrid(size);
+    initialSize = size;
+});
+
+function createGrid(size)
+{
+    for (let i=0; i<size; i++)
+    {
+        let rowDiv = document.createElement('div');
+        rowDiv.setAttribute('class', 'row');
+        container.appendChild(rowDiv);
+        
+        for (let j=0; j<size; j++)
+        {
+            let colDiv = document.createElement('div');
+            colDiv.setAttribute('class', 'unit');
+            rowDiv.appendChild(colDiv);
+        }
     }
 }
+
+function deleteGrid(size)
+{
+    for (let i=0; i<size; i++)
+    {
+        container.removeChild(document.querySelector('.row'));
+    }
+}
+
 
 container.addEventListener('mouseover', (e) => {
     if (e.target.getAttribute('class') == 'unit')
